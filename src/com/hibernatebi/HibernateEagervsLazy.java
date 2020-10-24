3d65b87e1037747176c698905d712821e81d3f82
vivekmanavalan <vivekmanavalan@github.com>
2020-10-24 19:56:54 +0530

    Eager vs Lazy loading

--- src/com/hibernatebi/HibernateEagervsLazy.java
+++ src/com/hibernatebi/HibernateEagervsLazy.java
@@ -4,10 +4,11 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class DeleteCourseBi {
+public class HibernateEagervsLazy {
 
 	public static void main(String[] args) {
 		
+		//Creating session factory
 		SessionFactory factory = new Configuration().configure("hibernate-config.xml")
 				.addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class).buildSessionFactory();
 		
@@ -19,28 +20,36 @@ public class DeleteCourseBi {
 		
 		try {
 			
+			//Begin transaction
 			session.beginTransaction();
 			
-			System.out.println("getting values from DB to delete");
-			Course temp = session.get(Course.class, 2);
+			System.out.println("Getting values from DB");
 			
+			Instructor temp = session.get(Instructor.class, 1);
 			
-			//Delete will only delete from the course table not from the instructor table because we did not cascade it
-			session.delete(temp);
-			
-			System.out.println("Deleted Successfully");
+			System.out.println("instructor table "+ temp);
 			
 			session.getTransaction().commit();
+			//closing the session so that the lazy loading throws error
+			
+			session.close();
+			
+			System.out.println("Course table" + temp.getCourses());
+			
+			
+			
+			
 		}
 		
-		catch(Exception e) {
+		catch(Exception e ) {
 			e.printStackTrace();
 		}
 		finally {
 			session.close();
 			factory.close();
 		}
-
+		
+		
 	}
 
 }
--- src/com/hibernatebi/Instructor.java
+++ src/com/hibernatebi/Instructor.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -41,7 +42,8 @@ public class Instructor {
 	  @JoinColumn(name="instructor_id") 
 	  private InstructorDetail instructorDetail;
 	  
-	  @OneToMany(mappedBy="instructor" ,cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	  //Here we explicitly specify EAGER because by default onetomany is LAZY
+	  @OneToMany(fetch = FetchType.LAZY, mappedBy="instructor" ,cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
 	  private List<Course> courses;
 	  
 	public Instructor () {
